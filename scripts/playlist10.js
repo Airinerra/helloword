34efee90a1d2f495f68523434f618783d4875c25
Airinerra <airinprimaferra@gmail.com>
2025-06-15 19:20:51 +0700

    Update playlist10.js

--- scripts/playlist10.js
+++ scripts/playlist10.js
@@ -1,21 +1,21 @@
 export const playlist = [
-  "../playlist/baleylot.mp3",
-  "../playlist/dolls.mp3",
-  "../playlist/ambahky.mp3",
-  "../playlist/entatani.mp3",
-  "../playlist/goryachaya.mp3",
-  "../playlist/hafdal.mp3",
-  "../playlist/hellokitty.mp3",
-  "../playlist/im.mp3",
-  "../playlist/living.mp3",
-  "../playlist/mahasiv.mp3",
-  "../playlist/muhur.mp3",
-  "../playlist/slishkim.mp3",
-  "../playlist/nifsi.mp3",
-  "../playlist/siahbo.mp3",
-  "../playlist/touta.mp3",
-  "../playlist/igrushka.mp3",
-  "../playlist/sentyabra.mp3"
+  "../../playlist/baleylot.mp3",
+  "../../playlist/dolls.mp3",
+  "../../playlist/ambahky.mp3",
+  "../../playlist/entatani.mp3",
+  "../../playlist/goryachaya.mp3",
+  "../../playlist/hafdal.mp3",
+  "../../playlist/hellokitty.mp3",
+  "../../playlist/im.mp3",
+  "../../playlist/living.mp3",
+  "../../playlist/mahasiv.mp3",
+  "../../playlist/muhur.mp3",
+  "../../playlist/slishkim.mp3",
+  "../../playlist/nifsi.mp3",
+  "../../playlist/siahbo.mp3",
+  "../../playlist/touta.mp3",
+  "../../playlist/igrushka.mp3",
+  "../../playlist/sentyabra.mp3"
   ];
 
 export const titles = [
