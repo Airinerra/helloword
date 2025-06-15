fdb087377906637e1bb053bb9f29a91526c3c1cd
Airinerra <airinprimaferra@gmail.com>
2025-06-15 18:31:46 +0700

    Create playlist10.js

---
+++ scripts/playlist10.js
@@ -0,0 +1,39 @@
+export const playlist = [
+  "../playlist/baleylot.mp3",
+  "../playlist/dolls.mp3",
+  "../playlist/ambahky.mp3",
+  "../playlist/entatani.mp3",
+  "../playlist/goryachaya.mp3",
+  "../playlist/hafdal.mp3",
+  "../playlist/hellokitty.mp3",
+  "../playlist/im.mp3",
+  "../playlist/living.mp3",
+  "../playlist/mahasiv.mp3",
+  "../playlist/muhur.mp3",
+  "../playlist/slishkim.mp3",
+  "../playlist/nifsi.mp3",
+  "../playlist/siahbo.mp3",
+  "../playlist/touta.mp3",
+  "../playlist/igrushka.mp3",
+  "../playlist/sentyabra.mp3"
+  ];
+
+export const titles = [
+  "Bolelut",
+  "Dolls",
+  "Eam Behky Me Halle",
+  "Enta Teni",
+  "Goryacheya Gremucheya",
+  "Hafdel",
+  "Hello Kitti",
+  "Jerusalem",
+  "Living Hell",
+  "Ma Ashiv Lecha",
+  "Mehur",
+  "My Sleshkim Raznyye",
+  "Nefsi Aoulhalek",
+  "Sheyohavu Ote Kecha",
+  "Teuta",
+  "Ya Ne Igreshka",
+  "Ze Sentyebreya"
+  ];
